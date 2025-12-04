624f739dc3b333993b5149ae0111222635702f6c
JoJoJoDoDoDo <itsoref2028@kehillahstudent.org>
2025-12-04 09:22:24 -0800

    hello

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
 from simulator import robot, FORWARD, BACKWARD, STOP
 
 # TODO: Write your code here!
+
+
+for i in range(100):
+    robot.motors(left = FORWARD, right = BACKWARD, seconds = 0.09)
+    robot.motors(left = FORWARD, right = FORWARD, seconds = 0.26)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Use robot.motors() to move
 # Use robot.left_sonar() and robot.right_sonar() to sense obstacles
 
