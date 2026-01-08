dca92832980f3f73263946cdddc6f7427f28ed99
JoJoJoDoDoDo <itsoref2028@kehillahstudent.org>
2026-01-08 08:43:02 -0800

    hello

--- main.py
+++ main.py
@@ -3,64 +3,79 @@ import random
 # TODO: Write your code here!
 #robot moves at 6 centimeters per second
 
-def draw_circle_right():
+def draw_circle_right(foc):
     "This function draws a circle to the right"
-    for i in range(30):
-        robot.motors(left = FORWARD, right = FORWARD, seconds = 0.1)
-        robot.motors(left = BACKWARD, right = FORWARD, seconds = 0.2)   
+    howmany = int(input("How many circles would you like to draw"))
+    for i in range(int(150 * howmany * foc)):
+        left_sensor = robot.left_sonar()
+        right_sensor = robot.right_sonar()
+        if left_sensor > 10 and right_sensor > 10:
+            robot.motors(left = FORWARD, right = FORWARD, seconds = 0.5)
+            robot.motors(left = BACKWARD, right = FORWARD, seconds = 1)
+        else:
+            print("stop bruh")
+            robot.motors(BACKWARD, BACKWARD, 3)
+            break
 def draw_square():
     "This function draws a perfect square"
     for i in range(4):
-        robot.motors(FORWARD, FORWARD, 5)
-        robot.motors(FORWARD, BACKWARD, 1.525)
-        
+        left_sensor = robot.left_sonar()
+        right_sensor = robot.right_sonar()
+        if left_sensor > 32 and right_sensor > 32:
+            robot.motors(FORWARD, FORWARD, 5)
+            robot.motors(FORWARD, BACKWARD, 1.525)
+        else:
+            print("I AM TOO CLOSE. STOP ME")
+            robot.motors(BACKWARD, BACKWARD, 3)
+            break
+
+
 # def go_random():
 #     for i in range(50):
 #         random.randint(1,50)
-def dvd(time):
+def dvd():
     "This function makes the robot go in a dvd-logo like motion around the box without touching or hitting the walls or corners"
     robot.motors(FORWARD, BACKWARD, 1)
     while True:
-        print(f"iteration #{i}")
-        left_sensor = robot.left_sonar()
-        right_sensor = robot.right_sonar()
-        if left_sensor < right_sensor:
-            robot.motors(FORWARD, FORWARD, (left_sensor/6)-1)
-            robot.motors(BACKWARD, FORWARD, 2)
-        else:
-            robot.motors(FORWARD, FORWARD, (right_sensor/6)-1)
-            robot.motors(FORWARD, BACKWARD, 2)
+        for i in range(5):
+            left_sensor = robot.left_sonar()
+            right_sensor = robot.right_sonar()
+            if left_sensor < right_sensor:
+                robot.motors(FORWARD, FORWARD, (left_sensor/6)-1)
+                robot.motors(BACKWARD, FORWARD, 2)
+            elif right_sensor < left_sensor:
+                robot.motors(FORWARD, FORWARD, (right_sensor/6)-1)
+                robot.motors(FORWARD, BACKWARD, 2)
+            elif left_sensor == right_sensor:
+                robot.motors(BACKWARD, BACKWARD, 1.2)
+                robot.motors(FORWARD, BACKWARD, 0.9)
+    question()
 
 
 
-
-draw_circle_right()
-draw_square()
-dvd(5000)
-
-
-
-
-user_choice = input("Select a choice: Square, Circle, DVD")
-if user_choice == "Circle":
-    draw_circle_right()
-elif user_choice == "Square":
-    draw_square()
-elif user_choice == "DVD":
-    dvd(5000)
+def keos():
+    "This is the menu that will pop up when you run the code and you contol what you want robot tot do"
+    user_choice = input("Select a choice: Square, Circle, DVD, Half Circle")
+    if user_choice == "Circle":
+        draw_circle_right(1)
+    elif user_choice == "Square":
+        draw_square()
+    elif user_choice == "DVD":
+        dvd()
+    elif user_choice == "Half Circle":
+        draw_circle_right(0.5)
 
 
+def question():
+    response = int(input("Would you like to 1. Continue  2. Stop and go back to menu  3. Quit"))
+    if response == 1:
+      dvd()
+    if response == 2:
+      keos()
+    if response == 3:
+       quit
 
-
-
-
-
-
-
-
-
-
-
+keos()
 
 # print(robot.left_sonar())  <- this all tests how fast the robot is in centimeters per second
 # print(robot.right_sonar())
@@ -72,15 +87,7 @@ elif user_choice == "DVD":
 
 
 
-
 # 6 centimeters per second/ CPS
-
-
-
-
-
-
-
 
 
 
